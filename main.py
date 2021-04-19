cae0068f8691ddddacc6b6cc93350fb7dd96b8de
shahargl <shaharglazner@gmail.com>
2021-04-19 10:03:59 +0300

    .

--- main.py
+++ main.py
@@ -69,6 +69,9 @@ def main():
             "Authorization": f"token {github_token}"
         })
         if not r.ok:
+            print("XXX")
+            print(r.reason)
+            print("XXX")
             output = "Failed to download logs"
             print(f"Error: {output}")
             print(f"::set-output name=result::{output}")
